4e1fb66cf2b3006bb2934a8232499fe8e574b05e
i.lago <i.lago@udc.es>
2023-11-15 19:31:58 +0100

    arreglar grafica regresion simple

--- regresionsimplemultiple.py
+++ regresionsimplemultiple.py
@@ -46,23 +46,40 @@ def bondad_ajuste(X, Y):
     return R_cuadrado
 
 def imprimir_datos(X, Y, x_nombre, y_nombre, x_nuevo=None):
-    n = X.shape[1]
-    _, axes = plt.subplots(n, 1, figsize=(8, 6 * n))
-    
-    for i in range(n):
-        x = pd.DataFrame({'X': X.iloc[:, i]})  # x tiene que ser un DataFrame, no DataSeries
+    n=X.shape[1]
+    if n==1:
+        x = pd.DataFrame({'X': X.iloc[:, 0]})
         recta = recta_regresion(x, Y)
-        axes[i].scatter(X.iloc[:, i], Y, color='blue', label=f'Datos de entrenamiento', s=1)
-        axes[i].plot(X.iloc[:, i], recta, color='black', label=f'Recta de Regresión', linewidth=1)
+        plt.scatter(X.iloc[:, 0], Y, color='blue', label=f'Datos de entrenamiento', s=1)
+        plt.plot(X.iloc[:, 0], recta, color='black', label=f'Recta de Regresión', linewidth=1)
 
         if x_nuevo is not None:
-            x_nuevo2 = pd.DataFrame({'X': x_nuevo.iloc[:, i]})
+            x_nuevo2 = pd.DataFrame({'X': x_nuevo.iloc[:, 0]})
             y_nuevo = recta_regresion(x, Y, x_nuevo2)
-            axes[i].scatter(x_nuevo2, y_nuevo, color='red', label=f'Predicciones', s=20) 
+            plt.scatter(x_nuevo2, y_nuevo, color='red', label=f'Predicciones', s=20) 
 
-        axes[i].set_xlabel(x_nombre[i])
-        axes[i].set_ylabel(y_nombre)
-        axes[i].legend()
+        plt.xlabel(x_nombre[0])
+        plt.ylabel(y_nombre)
+        plt.legend()
+        plt.show()
+    else:
+        n = X.shape[1]
+        _, axes = plt.subplots(n, 1, figsize=(8, 6 * n))
+        
+        for i in range(n):
+            x = pd.DataFrame({'X': X.iloc[:, i]})  # x tiene que ser un DataFrame, no DataSeries
+            recta = recta_regresion(x, Y)
+            axes[i].scatter(X.iloc[:, i], Y, color='blue', label=f'Datos de entrenamiento', s=1)
+            axes[i].plot(X.iloc[:, i], recta, color='black', label=f'Recta de Regresión', linewidth=1)
 
-    plt.tight_layout()
-    plt.show()
+            if x_nuevo is not None:
+                x_nuevo2 = pd.DataFrame({'X': x_nuevo.iloc[:, i]})
+                y_nuevo = recta_regresion(x, Y, x_nuevo2)
+                axes[i].scatter(x_nuevo2, y_nuevo, color='red', label=f'Predicciones', s=20) 
+
+            axes[i].set_xlabel(x_nombre[i])
+            axes[i].set_ylabel(y_nombre)
+            axes[i].legend()
+
+        plt.tight_layout()
+        plt.show()
