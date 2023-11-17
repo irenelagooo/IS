b11e3231c280c4164b72af7e0725400551d94b10
i.lago <i.lago@udc.es>
2023-11-17 18:15:34 +0100

    funcion recta

--- funcion recta con m y n.py
+++ funcion recta con m y n.py
@@ -1,14 +1,6 @@
-def calcular_recta_regresion(m, n, x_values):
-    """
-    Calcula la recta de regresión lineal dados los parámetros m y n.
+import matplotlib.pyplot as plt
+import numpy as np
 
-    Parámetros:
-    - m: Pendiente de la recta de regresión.
-    - n: Ordenada al origen de la recta de regresión.
-    - x_values: Valores de la variable X para los cuales se calculará la recta.
-
-    Devuelve:
-    - y_values: Valores de la variable Y correspondientes a la recta de regresión.
-    """
-    y_values = [m * x + n for x in x_values]
-    return y_values
+def calcular_recta_regresion(m, n, x):
+    x=list(range(200))
+    return m * x + n
