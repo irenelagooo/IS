32a959b5cdac3886b345e374b01edd1cfaf9b23f
a.exposito.cainzos <a.exposito.cainzos@udc.es>
2023-11-27 13:10:31 +0100

    cuadros texto

--- GUI nueva.py
+++ GUI nueva.py
@@ -42,8 +42,8 @@ def boton_predicciones(root,x_seleccionadas):
     
 
 def calcular_predicciones_click(root,x_seleccionadas):
-    for entry in cuadros_texto:
-        entry.destroy()
+    '''for entry in cuadros_texto:
+        entry.destroy()'''
     
     cuadros_texto = [] 
     
@@ -112,7 +112,6 @@ def seleccionar_x(root,columnas_numericas):
     seleccionar_var_x_label = tk.Label(root, text="Selecciona variable(s) X")
     seleccionar_var_x_label.place(x=100, y=300)
 
- 
     canvas_x = tk.Canvas(root, bd=0, highlightthickness=0)
     canvas_x.place(x=300, y=300)
 
