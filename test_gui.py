dce7e97a3c665f0fbfd2ea787a235697ab6ddd6f
irenelagooo <i.lago@udc.es>
2023-12-26 19:58:25 +0100

    fix: arregla test_leer_archivos y test_regresion_gui

--- test_gui.py
+++ test_gui.py
@@ -6,9 +6,11 @@ from carga_guardado import leer_archivos
 from GUI_mod import regresion_gui
 from regresionsimplemultiple import predicciones
 
-@pytest.mark.parametrize("archivo",[(""),("C:\Users\Raúl\Desktop\IA\3 cuatri\Enx Software\P1\housing.csv"),
-                                        ("C:\Users\irene\Desktop\IA\ES\IS\housing.db"),
-                                        ('C:\Users\alexe\OneDrive\Escritorio\IS\housing.xlsx')])
+@pytest.mark.parametrize("archivo", [(""),
+    (r"C:\Users\Raúl\Desktop\IA\3 cuatri\Enx Software\P1\housing.csv"),
+    (r"C:\Users\irene\Desktop\IA\ES\IS\housing.db"),
+    (r"C:\Users\alexe\OneDrive\Escritorio\IS\housing.xlsx")
+])
 
 def test_leer_archivos(archivo):
     assert len(archivo)>0
@@ -17,20 +19,22 @@ def test_leer_archivos(archivo):
     assert datos is not None
     assert len(datos) > 0
 
+root=tk.Tk()
 @pytest.mark.parametrize("variables_x, y_seleccionada",
                          [({'longitud':True, 'latitud': True, 'habitantes': False},tk.StringVar(value='latitud')),
                            ({'longitud':False, 'latitud': False, 'habitantes': False},tk.StringVar(value='habitantes')), 
                            ({'longitud':True, 'latitud': False, 'habitantes': False},tk.StringVar())])
 
-def test_regresion_gui(variables_x, y_seleccionada):
+def test_regresion_gui(root,variables_x, y_seleccionada):
     mis_datos = pd.DataFrame({'longitud': [1, 2, 223, 4616, 5],'latitud':[3,4,5,6,1],'habitantes':[100,30,40,1,0]})
-    root=crear_ventana()
+
     variable_y=tk.StringVar()
     resultado_label=tk.Label(root)
-    regresion_gui(mis_datos,variables_x, y_seleccionada,resultado_label)
-
     assert variable_y.get() != ''
     assert any(variables_x.values())
+    regresion_gui(mis_datos,variables_x, y_seleccionada,resultado_label)
+
+    
 
 @pytest.mark.parametrize("m, n, x", [([1, 2, 3], 4, [4,5,4]),
     ([2, 3, 4, 7], 4, ['str',3,6,9]),
@@ -41,3 +45,4 @@ def test_predicciones(m, n, x):
     for i in x:
         assert isinstance(i, float)
     assert isinstance(resultado, float)
+
